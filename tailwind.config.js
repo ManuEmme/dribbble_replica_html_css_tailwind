a267b5e4c0e9700ad4cf1b773bbe4e255cd749b3
ManuEmme <marascoemanuel1@gmail.com>
2024-02-07 13:07:59 +0100

    ad footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,46 +1,69 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    theme: {
-      screens: {
-        'tablet': '640px',
-        // => @media (min-width: 640px) { ... }
-  
-        'laptop': '1024px',
-        // => @media (min-width: 1024px) { ... }
-  
-        'desktop': '1280px',
-        // => @media (min-width: 1280px) { ... }
-      },
-    }
+  theme: {
+    screens: {
+      'tablet': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'laptop': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
   }
-  /** @type {import('tailwindcss').Config} */
+}
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    theme: {
-      screens: {
-        'sm': '640px',
-        // => @media (min-width: 640px) { ... }
-  
-        'md': '768px',
-        // => @media (min-width: 768px) { ... }
-  
-        'lg': '1024px',
-        // => @media (min-width: 1024px) { ... }
-  
-        'xl': '1280px',
-        // => @media (min-width: 1280px) { ... }
-  
-        '2xl': '1536px',
-        // => @media (min-width: 1536px) { ... }
+  theme: {
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1200px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    }
+  }
+}
+module.exports = {
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('/img/hero-pattern.svg')",
+        'footer-texture': "url('/img/footer-texture.png')",
       }
     }
   }
-  module.exports = {
-    theme: {
-      extend: {
-        backgroundImage: {
-          'hero-pattern': "url('/img/hero-pattern.svg')",
-          'footer-texture': "url('/img/footer-texture.png')",
-        }
-      }
-    }
-  }
+}
+
+ 
+// /**@type {import('tailwindcss').Config} */
+// module.exports = {
+   
+//   theme: {
+//     extend: {
+//       keyframes: {
+//         badgeColorCycle: {
+//           '0%': { backgroundColor: '#ffda79' },
+
+//           '25%': { backgroundColor: '#ffabe7' },
+
+//           '50%': { backgroundColor: '#d2dcf3' },
+
+//           '75%': { backgroundColor: '#edf3d8' },
+
+//           '100%': { backgroundColor: '#ffda79' }
+//         }
+//       }
+//     }
+//   }
+// }
